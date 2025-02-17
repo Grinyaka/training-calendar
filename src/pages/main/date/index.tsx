2f5410ef26439c77a15272c25f9f5fef218ddfd1
Grinyaka <grinyaka13@gmail.com>
2025-02-17 18:50:32 +0500

    Output date with week day

--- src/pages/main/date/index.tsx
+++ src/pages/main/date/index.tsx
@@ -1,10 +1,11 @@
-import {useParams} from 'react-router'
+import moment from 'moment'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
 import styled from 'styled-components'
-import {PageContainer} from '../../../components/PageContainer'
-import Notes from './Notes'
-import {useEffect, useState} from 'react'
-import {useStoreMain} from '../../../store'
+import { PageContainer } from '../../../components/PageContainer'
+import { useStoreMain } from '../../../store'
 import ActivitiesList from './ActivitiesList'
+import Notes from './Notes'
 import TimePicker from './TimePicker'
 
 const Wrapper = styled(PageContainer)`
@@ -15,7 +16,11 @@ const Wrapper = styled(PageContainer)`
   gap: 15px;
 `
 const Title = styled.span`
-  font-size: ${({theme}) => theme.fontSizes.xlarge};
+  font-size: clamp(
+    ${({theme}) => theme.fontSizes.large},
+    3vw,
+    ${({theme}) => theme.fontSizes.xlarge}
+  );
   color: ${({theme}) => theme.textColors.secondary};
   font-weight: bold;
   padding: 5px 10px;
@@ -48,22 +53,30 @@ const DatePage = () => {
   const {date} = useParams()
   const [isLoading, setIsLoading] = useState(true)
   const {setDayData} = useStoreMain((state) => state.actions)
-  const {dayData} = useStoreMain((state) => state)
+  const dayData = useStoreMain((state) => state.dayData)
+  const currentMonth = useStoreMain((state) => state.currentMonth)
+
+  // TODO: add year (not important until 2026)
+  const currentDate = `2025-${currentMonth}-${date}`
+  const dayOfWeek = moment(currentDate).format('dddd')
+  const month = moment(`2025-${currentMonth}-${date}`).format('MMMM')
 
   useEffect(() => {
     setDayData(Number(date) - 1)
     setIsLoading(false)
   }, [])
-  
+
   return (
     <Wrapper>
-      <Title>{date}</Title>
+      <Title>
+        {month} {date}, {dayOfWeek}
+      </Title>
       {isLoading ? (
         <div>Loading...</div>
       ) : (
         <>
           <DataRow>
-          <TimePicker from={dayData.from} to={dayData.to} />
+            <TimePicker from={dayData.from} to={dayData.to} />
           </DataRow>
           <ActivitiesList day={dayData.day - 1} />
           <Notes day={dayData.day - 1} currentNotes={dayData.notes} />
